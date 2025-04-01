42174452fe4ceab3d19d627a9dd5151d485383f3
floeschau <frank.loeschau@terradue.com>
2025-04-01 16:38:40 +0200

    Set arbitrary_types_allowed = True (to avoid PydanticSchemaGenerationError)

--- src/ogcapi_processes_client/models/input_value_no_object.py
+++ src/ogcapi_processes_client/models/input_value_no_object.py
@@ -16,7 +16,7 @@
 from __future__ import annotations
 import json
 import pprint
-from pydantic import BaseModel, ConfigDict, Field, StrictBool, StrictBytes, StrictFloat, StrictInt, StrictStr, ValidationError, field_validator
+from pydantic import BaseModel as PydanticBaseModel, ConfigDict, Field, StrictBool, StrictBytes, StrictFloat, StrictInt, StrictStr, ValidationError, field_validator
 from typing import Any, List, Optional, Union
 from ogcapi_processes_client.models.bbox import Bbox
 from pydantic import StrictStr, Field
@@ -24,6 +24,10 @@ from typing import Union, List, Set, Optional, Dict
 from typing_extensions import Literal, Self
 
 INPUTVALUENOOBJECT_ONE_OF_SCHEMAS = ["Bbox", "List[str]", "bool", "bytearray", "float", "int", "str"]
+
+class BaseModel(PydanticBaseModel):
+    class Config:
+        arbitrary_types_allowed = True
 
 class InputValueNoObject(BaseModel):
     """
